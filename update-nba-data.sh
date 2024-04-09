0080137209e161268fd1d67c82ab41e288afc9ca
fuzmik <75134092+fuzmik@users.noreply.github.com>
2024-04-09 16:09:30 -0700

    Add files via upload

---
+++ update-nba-data.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Create dataset with the latest data for 2023-24 season
+cd src/Process-Data
+python -m Get_Data
+python -m Get_Odds_Data
+python -m Create_Games
+
+# Train models
+cd ../Train-Models
+python -m XGBoost_Model_ML
+python -m XGBoost_Model_UO
