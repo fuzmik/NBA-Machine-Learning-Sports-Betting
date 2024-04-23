d7f128fb4d6b75623c2ec450e97364d75ef1ec4e
frank <sljbbr@gmail.com>
2024-04-22 18:10:08 -0700

    trained

--- src/Predict/NN_Runner.py
+++ src/Predict/NN_Runner.py
@@ -9,8 +9,8 @@ from src.Utils import Expected_Value
 from src.Utils import Kelly_Criterion as kc
 
 init()
-model = load_model('Models/NN_Models/Trained-Model-ML-1699315388.285516')
-ou_model = load_model("Models/NN_Models/Trained-Model-OU-1699315414.2268295")
+model = load_model('Models/NN_Models/Trained-Model-ML-1713833521.1602352')
+ou_model = load_model("Models/NN_Models/Trained-Model-OU-1713833564.3375854")
 
 
 def nn_runner(data, todays_games_uo, frame_ml, games, home_team_odds, away_team_odds, kelly_criterion):
